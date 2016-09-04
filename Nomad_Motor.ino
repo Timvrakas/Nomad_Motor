b4998b776746e88fc3262fbf6fd7044a57498c50
Tim Vrakas <tim.vrakas@gmail.com>
2016-09-03 22:10:38 -0500

    codez

--- Nomad_Motor.ino
+++ Nomad_Motor.ino
@@ -6,6 +6,11 @@ const int limitPin = 0; //NEED
 
 const int LOW_POS = 100;
 const long HIGH_POS = 5000;
+unsigned long pos;
+boolean dir;
+
+const bool UP = 1;
+const bool DOWN = 0;
 
 void setup() {
   pinMode(pinEnable, OUTPUT);
@@ -13,76 +18,57 @@ void setup() {
   pinMode(pinDir, OUTPUT);
 
   digitalWrite(pinEnable, LOW);
-  digitalWrite(pinDir, HIGH);
 
   delay(1000);
   long stepCount = 0;
 
-
   //Lower until limit switch is triggered
-  int pulseTime=10;
-  while(digitalRead(limitPin)==LOW){
-    digitalWrite(pinStepLeft, HIGH);
-    digitalWrite(pinStepRight, HIGH);
+  dir = DOWN;
+  digitalWrite(pinDir, dir);
+  int pulseTime = 10;
+  while (digitalRead(limitPin) == LOW) {
+    digitalWrite(pinStep, HIGH);
     delayMicroseconds(pulseTime);
-    digitalWrite(pinStepLeft, LOW);
-    digitalWrite(pinStepRight, LOW);
-    delayMicroseconds(pulseTime);
-  }
-
-//Raise to "Low" position
-for(int i=0; i<LOW_POS; i++){
-    digitalWrite(pinStepLeft, HIGH);
-    digitalWrite(pinStepRight, HIGH);
-    delayMicroseconds(pulseTime);
-    digitalWrite(pinStepLeft, LOW);
-    digitalWrite(pinStepRight, LOW);
+    digitalWrite(pinStep, LOW);
     delayMicroseconds(pulseTime);
   }
 
-
-
-  double rate = startingRate;
-  while(rate < finalRate){
-    int pulseTime = (int)(1000000.0/rate);
-    digitalWrite(pinStepLeft, HIGH);
-    digitalWrite(pinStepRight, HIGH);
+  //Raise to "Low" position
+  dir = UP;
+  pulseTime = 10;
+  digitalWrite(pinDir, dir);
+  for (int i = 0; i < LOW_POS; i++) {
+    digitalWrite(pinStep, HIGH);
     delayMicroseconds(pulseTime);
-    digitalWrite(pinStepLeft, LOW);
-    digitalWrite(pinStepRight, LOW);
+    digitalWrite(pinStep, LOW);
     delayMicroseconds(pulseTime);
-    stepCount++;
-    rate += accelRate;
   }
-
-  while(stepCount < endCruise){
-    int pulseTime = (int)(1000000.0/finalRate);
-    digitalWrite(pinStepLeft, HIGH);
-    digitalWrite(pinStepRight, HIGH);
-    delayMicroseconds(pulseTime);
-    digitalWrite(pinStepLeft, LOW);
-    digitalWrite(pinStepRight, LOW);
-    delayMicroseconds(pulseTime);
-    stepCount++;
-    delayMicroseconds(20);
-  }
-
-  rate = finalRate;
-  while(rate > startingRate){
-    int pulseTime = (int)(1000000.0/rate);
-    digitalWrite(pinStepLeft, HIGH);
-    digitalWrite(pinStepRight, HIGH);
-    delayMicroseconds(pulseTime);
-    digitalWrite(pinStepLeft, LOW);
-    digitalWrite(pinStepRight, LOW);
-    delayMicroseconds(pulseTime);
-    stepCount++;
-    rate -= accelRate;
-  }
+  pos = 0;
 }
 
-void loop(){
+void loop() {
+  //convert to radians
+  float angle = float(map(pos, LOW_POS, HIGH_POS, 0, 180)) / 114.59;
+  long pulseTime = 1000000 / long((sin(angle) + 0.1));
+  int increment;
+  if (dir == UP) {
+    increment = 1;
+  } else {
+    increment = -1;
+  }
 
+  digitalWrite(pinDir, dir);
+  for (int i = 0; i < 100; i++) {
+    digitalWrite(pinStep, HIGH);
+    delayMicroseconds(pulseTime);
+    digitalWrite(pinStep, LOW);
+    delayMicroseconds(pulseTime);
+    pos += increment;
+  }
 
+  if (pos >= HIGH_POS) {
+    dir = DOWN;
+  } else if (pos <= LOW_POS) {
+    dir = UP;
+  }
 }
-
